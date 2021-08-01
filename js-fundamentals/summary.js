2159a0298b0184e1dd14081ea91fc9db8958cad1
sumaiya-Ritu <sumaiyaritu9727@gmail.com>
2021-08-01 22:38:36 +0600

    js-fundamentals conditions

---
+++ js-fundamentals/summary.js
@@ -0,0 +1,31 @@
+var tableaLength = 12;
+var tourDestination = ['coxbazar', 'nepal', 'bhutan', 'paris'];
+
+tourDestination.indexOf('paris');
+var fourthDestination = tourDestination[3];
+tourDestination[1] = 'Srilanka';
+
+tourDestination.push('London');
+tourDestination.pop();
+
+if (tourDestination[1] == 'nepal') {
+    console.log('pahare ahare ahare');
+}
+else if (tourDestination[1] == 'china') {
+    console.log('china ami jabona');
+}
+else if (tourDestination.tableaLength == 4) {
+    console.log('ghurmu ar ghumru');
+}
+else {
+    console.log('jabona kothaw ');
+}
+
+var eggPrice = 32;
+var myBudget = 32;
+if (eggPrice <= myBudget) {
+    console.log('ami dim khabo');
+}
+if (eggPrice >= myBudget) {
+    console.log('ami dim khabo');
+}
