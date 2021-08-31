afeac67f89db9da3344ffbcbc196bbbb5d28f426
sumaiya-Ritu <sumaiyaritu9727@gmail.com>
2021-09-01 01:17:47 +0600

    es6

---
+++ ES6-Intro/basic.js
@@ -0,0 +1,32 @@
+// if else condition bujhaise
+
+// array
+
+const num = [24, 55, 90, 31, 1];
+const numbercount = num.length;
+num.push(111);
+num.pop();
+num[2] = 555;
+
+// check whether 222 included inarray
+if (num.indexOf(222) != -1) {
+
+}
+if (num.includes(222)) {
+
+}
+
+// loop
+for (const number of num) {
+
+}
+// fucntion
+function fullName(first, Second) {
+    const name = first + ' ' + second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// object
+const bottle = {
+    color: 'yellow'
+}
